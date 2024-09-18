daf5a866c9a248193858a894ec892ce01766e891
Engrdude <141180972+Engrdude@users.noreply.github.com>
2024-09-19 01:37:30 +0530

    Time: 12 ms (35.86%) | Memory: 49.4 MB (79.32%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -3,11 +3,11 @@ class Solution {
         HashMap<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        for(int i=0;i<nums.length;i++)
-        {
-            if(map.get(nums[i])>nums.length/2)
-            return nums[i];
-        }
+       for(Map.Entry<Integer,Integer>p:map.entrySet())
+       {
+       if(p.getValue()>nums.length/2)
+       return p.getKey();
+       }
         return 0;
         
     }
