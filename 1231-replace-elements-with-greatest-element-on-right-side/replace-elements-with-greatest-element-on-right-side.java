8b773b957eeb449cb6501392f687e233ee89cb4c
Engrdude <141180972+Engrdude@users.noreply.github.com>
2024-09-21 01:04:58 +0530

    Time: 292 ms (28.21%) | Memory: 46.6 MB (36.88%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -1,16 +1,16 @@
 class Solution {
-    public static int greatest_elem(int []nums,int s)
+   /* public static int greatest_elem(int []nums,int s)
     {
-        int max=nums[s];
+       int max=nums[s];
         for(int i=s+1;i<nums.length;i++)
         {
             if(nums[i]>max)
             max=nums[i];
         }
         return max;
-    }
+    }*/
     public int[] replaceElements(int[] arr) {
-        int [] ans=new int[arr.length];
+       /* int [] ans=new int[arr.length];
         int i=0;
         for(i=0;i<arr.length-1;i++)
         {
@@ -19,6 +19,21 @@ class Solution {
         }
         ans[i]=-1;
          
+         return ans;*/
+
+         int max=Integer.MIN_VALUE;
+         int []ans =new int[arr.length];
+         ans[arr.length-1]=-1;
+         for(int i=arr.length-1;i>0;i--)
+         {
+            if(arr[i]>max){
+                max=arr[i];
+                ans[i-1]=max;
+            }
+            else
+            ans[i-1]=max;
+         }
          return ans;
+
     }
 }
